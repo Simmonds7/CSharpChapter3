82cddbf1bedb4bd752aaa7a8996a455ca90fb0c7
Simmonds7 <44840687+Simmonds7@users.noreply.github.com>
2018-11-13 12:33:48 -0600

    New program (Car rental)

---
+++ CarRentalInteractiveGUI/CarRentalInteractiveGUI/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CarRentalInteractiveGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDays = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMiles = new System.Windows.Forms.TextBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.lblReciept = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(391, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How many days have you rented the vehicle?";
+            // 
+            // txtDays
+            // 
+            this.txtDays.Location = new System.Drawing.Point(17, 41);
+            this.txtDays.Name = "txtDays";
+            this.txtDays.Size = new System.Drawing.Size(100, 20);
+            this.txtDays.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(406, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "How many miles did you drive with the vehicle?";
+            // 
+            // txtMiles
+            // 
+            this.txtMiles.Location = new System.Drawing.Point(17, 109);
+            this.txtMiles.Name = "txtMiles";
+            this.txtMiles.Size = new System.Drawing.Size(100, 20);
+            this.txtMiles.TabIndex = 3;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrint.Location = new System.Drawing.Point(17, 157);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(132, 39);
+            this.btnPrint.TabIndex = 4;
+            this.btnPrint.Text = "Print Reciept";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // lblReciept
+            // 
+            this.lblReciept.AutoSize = true;
+            this.lblReciept.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReciept.Location = new System.Drawing.Point(17, 259);
+            this.lblReciept.Name = "lblReciept";
+            this.lblReciept.Size = new System.Drawing.Size(0, 25);
+            this.lblReciept.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblReciept);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.txtMiles);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDays);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Car Rental";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDays;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMiles;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Label lblReciept;
+    }
+}
+
---
+++ CarRentalInteractiveGUI/CarRentalInteractiveGUI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRentalInteractiveGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            double days;
+            double miles;
+            double total;
+
+            days = 20 * Convert.ToDouble(txtDays.Text);
+            miles = .25 * Convert.ToDouble(txtMiles.Text);
+            total = days + miles;
+
+            lblReciept.Text = "Thank you for renting a car from Frank's car rentals! \n Your total is $" + total;
+
+
+            
+
+
+        }
+    }
+}

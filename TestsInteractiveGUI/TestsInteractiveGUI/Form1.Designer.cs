a160e560c0e39bdaea0fe8b5932896e33b1187b6
Simmonds7 <44840687+Simmonds7@users.noreply.github.com>
2018-11-13 13:16:26 -0600

    tests interactive

---
+++ TestsInteractiveGUI/TestsInteractiveGUI/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TestsInteractiveGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtScore1 = new System.Windows.Forms.TextBox();
+            this.txtScore2 = new System.Windows.Forms.TextBox();
+            this.txtScore4 = new System.Windows.Forms.TextBox();
+            this.txtScore3 = new System.Windows.Forms.TextBox();
+            this.txtScore5 = new System.Windows.Forms.TextBox();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter 5 Test Scores";
+            // 
+            // txtScore1
+            // 
+            this.txtScore1.Location = new System.Drawing.Point(18, 53);
+            this.txtScore1.Name = "txtScore1";
+            this.txtScore1.Size = new System.Drawing.Size(55, 20);
+            this.txtScore1.TabIndex = 1;
+            // 
+            // txtScore2
+            // 
+            this.txtScore2.Location = new System.Drawing.Point(79, 53);
+            this.txtScore2.Name = "txtScore2";
+            this.txtScore2.Size = new System.Drawing.Size(55, 20);
+            this.txtScore2.TabIndex = 2;
+            // 
+            // txtScore4
+            // 
+            this.txtScore4.Location = new System.Drawing.Point(201, 53);
+            this.txtScore4.Name = "txtScore4";
+            this.txtScore4.Size = new System.Drawing.Size(55, 20);
+            this.txtScore4.TabIndex = 4;
+            // 
+            // txtScore3
+            // 
+            this.txtScore3.Location = new System.Drawing.Point(140, 53);
+            this.txtScore3.Name = "txtScore3";
+            this.txtScore3.Size = new System.Drawing.Size(55, 20);
+            this.txtScore3.TabIndex = 3;
+            // 
+            // txtScore5
+            // 
+            this.txtScore5.Location = new System.Drawing.Point(262, 53);
+            this.txtScore5.Name = "txtScore5";
+            this.txtScore5.Size = new System.Drawing.Size(55, 20);
+            this.txtScore5.TabIndex = 5;
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(18, 91);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(116, 23);
+            this.btnAverage.TabIndex = 6;
+            this.btnAverage.Text = "Calculate Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverage.Location = new System.Drawing.Point(18, 137);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(0, 24);
+            this.lblAverage.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.txtScore5);
+            this.Controls.Add(this.txtScore4);
+            this.Controls.Add(this.txtScore3);
+            this.Controls.Add(this.txtScore2);
+            this.Controls.Add(this.txtScore1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Test Score Average Program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtScore1;
+        private System.Windows.Forms.TextBox txtScore2;
+        private System.Windows.Forms.TextBox txtScore4;
+        private System.Windows.Forms.TextBox txtScore3;
+        private System.Windows.Forms.TextBox txtScore5;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Label lblAverage;
+    }
+}
+
---
+++ TestsInteractiveGUI/TestsInteractiveGUI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestsInteractiveGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            decimal test1;
+            decimal test2;
+            decimal test3;
+            decimal test4;
+            decimal test5;
+            decimal 
+
+            test1 = Convert.ToDecimal(txtScore1.Text);
+            test2 = Convert.ToDecimal(txtScore2.Text);
+            test3 = Convert.ToDecimal(txtScore3.Text);
+            test4 = Convert.ToDecimal(txtScore4.Text);
+            test5 = Convert.ToDecimal(txtScore5.Text);
+
+
+        }
+    }
+}

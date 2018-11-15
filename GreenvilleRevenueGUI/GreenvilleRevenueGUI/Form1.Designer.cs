18d8f70978c6c24b4285e7ee50af21858b3cb81c
Simmonds7 <44840687+Simmonds7@users.noreply.github.com>
2018-11-15 13:03:15 -0600

    add Greenville complete

---
+++ GreenvilleRevenueGUI/GreenvilleRevenueGUI/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GreenvilleRevenueGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContest1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtContest2 = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(455, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "How mant contestants were in last years competition?";
+            // 
+            // txtContest1
+            // 
+            this.txtContest1.Location = new System.Drawing.Point(478, 13);
+            this.txtContest1.Name = "txtContest1";
+            this.txtContest1.Size = new System.Drawing.Size(100, 20);
+            this.txtContest1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(344, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "How many contestants are in this years?";
+            // 
+            // txtContest2
+            // 
+            this.txtContest2.Location = new System.Drawing.Point(366, 54);
+            this.txtContest2.Name = "txtContest2";
+            this.txtContest2.Size = new System.Drawing.Size(100, 20);
+            this.txtContest2.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(16, 86);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(16, 158);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(269, 100);
+            this.lblResult.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtContest2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtContest1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Greenville Revenue";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtContest1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtContest2;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ GreenvilleRevenueGUI/GreenvilleRevenueGUI/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreenvilleRevenueGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double oldContest;
+            double newContest;
+            double revenue;
+
+            oldContest = Convert.ToDouble(txtContest1.Text);
+            newContest = Convert.ToDouble(txtContest2.Text);
+            revenue = newContest * 25;
+
+            if (newContest > oldContest)
+            {
+                lblResult.Text = "Your expected Revenue is $" + revenue + ".\n This years number of contestants is greater than last years.";
+            } 
+            else
+            {
+                lblResult.Text = "Your expected Revenue is $" + revenue + ".\n This years number of contestants is less than last years.";
+            }
+        }
+    }
+}
